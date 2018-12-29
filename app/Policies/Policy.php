032c144d41c35c3a9b091593a0231fed85f03738
kspaper_0 <15380324@qq.com>
2018-12-29 08:33:28 +0000

    Add manage contents permission and visit horizon dashboard permission

--- app/Policies/Policy.php
+++ app/Policies/Policy.php
@@ -8,15 +8,16 @@ class Policy
 {
     use HandlesAuthorization;
 
-    public function __construct()
-    {
-        //
-    }
-
     public function before($user, $ability)
-	{
-	    // if ($user->isSuperAdmin()) {
-	    // 		return true;
-	    // }
-	}
+    {
+        // 如果用户拥有管理内容的权限的话，即授权通过
+        if ($user->can('manage_contents')) {
+            return true;
+        }
+
+        //当此处未定义方法时，会先找寻TopicPolicy此方法的内容
+        //即，只有当前用户才能更改或删除自己的贴子
+        //此处，当权限为'manage_contents'后，由于返回的一直是 true
+        //将忽略其他的判断
+    }
 }
