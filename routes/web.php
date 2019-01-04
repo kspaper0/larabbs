815f5b37e4b80e86a98ba80a6a3513341a0c6842
kspaper_0 <15380324@qq.com>
2019-01-04 10:21:38 +0000

    Setup default home page

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,8 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+//Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 Auth::routes();
 //Laravel 用户认证路由, 内容如下：
